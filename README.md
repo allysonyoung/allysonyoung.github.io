0d4a096dd9e89efb69db903c250e9ec0da93ff66
allysonyoung <ayoung1009@gmail.com>
2017-11-14 01:03:14 -0500

    layout change and some progress

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Allyson Young
 personal website
+
+TODO: 
+[] mobile/tablet responsive
+[] project descriptions on hover
+[] add resume
---
+++ js/index.js
@@ -0,0 +1,4 @@
+window.sr = ScrollReveal({ reset: true });
+sr.reveal('.home-title', { duration: 1000, delay: 150 });
+sr.reveal('.about-content', { duration: 1000, viewFactor: 0.5, origin: 'top'});
+sr.reveal('.about-picture', { duration: 500, viewFactor: 0.3, origin: 'top'});
