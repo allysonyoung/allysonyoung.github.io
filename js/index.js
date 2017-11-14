d66198a3eede7b39866b0bcd81215e60ccc78edc
allysonyoung <ayoung1009@gmail.com>
2017-11-14 14:23:44 -0500

    add arrow, minor js and css changes

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
 window.sr = ScrollReveal({ reset: true });
-sr.reveal('.about-content', { duration: 1000, viewFactor: 0.5, origin: 'top'});
-sr.reveal('.about-picture', { duration: 500, viewFactor: 0.3, origin: 'top'});
+sr.reveal('.hi', { duration: 2000, viewFactor: 0.3, origin: 'top'});
+sr.reveal('.intro', { duration: 2000, viewFactor: 0.3, origin: 'bottom'});
+sr.reveal('.arrow-image', { duration: 2000, viewFactor: 0.3, delay: 700, origin: 'top'});
+
+function scrollNav() {
+  $('.anchor').click(function(){  
+    $('html, body').stop().animate({
+        scrollTop: $( $(this).attr('href') ).offset().top - 50
+    }, 1000);
+    return false;
+  });
+  $('.scrollTop a').scrollTop();
+}
+scrollNav();
